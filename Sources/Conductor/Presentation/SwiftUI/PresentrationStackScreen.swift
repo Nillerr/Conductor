fbb6a8542ec304500ec4856733e0484e437fbcd6
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2022-09-08 21:41:51 +0200

    Add PresentrationStackScreen

---
+++ Sources/Conductor/Presentation/SwiftUI/PresentrationStackScreen.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+public struct PresentrationStackScreen<Content: View>: View {
+    @Environment(\.presentationEntry) public var entry: PresentationEntry
+    
+    public let id: String
+    public let content: (AnyHashable) -> Content
+    
+    public init<Value>(_ type: Value.Type, @ViewBuilder content: @escaping (Value) -> Content) {
+        self.id = String(describing: type)
+        self.content = { value in content(value as! Value) }
+    }
+    
+    public var body: some View {
+        if entry.type == id {
+            content(entry.value)
+        } else {
+            EmptyView()
+        }
+    }
+}
