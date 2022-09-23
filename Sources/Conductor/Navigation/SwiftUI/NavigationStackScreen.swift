c0eb4a7a17321046225cc28e8a222b901c1d9c4d
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2022-09-23 12:54:06 +0200

    Add onDismiss

--- Sources/Conductor/Navigation/SwiftUI/NavigationStackScreen.swift
+++ Sources/Conductor/Navigation/SwiftUI/NavigationStackScreen.swift
@@ -2,18 +2,26 @@ import SwiftUI
 
 public struct NavigationStackScreen<Content: View>: View {
     @Environment(\.navigationEntry) public var entry: NavigationEntry
+    @Environment(\.navigationRouter) public var navigation: NavigationRouter
     
     public let type: TypeId
+    public let onDismiss: () -> Void
     public let content: (Any) -> Content
     
-    public init<Value>(_ type: Value.Type, @ViewBuilder content: @escaping (Value) -> Content) {
+    public init<Value>(_ type: Value.Type, onDismiss: @escaping () -> Void = {}, @ViewBuilder content: @escaping (Value) -> Content) {
         self.type = toTypeIdentifier(type)
+        self.onDismiss = onDismiss
         self.content = { value in content(value as! Value) }
     }
     
     public var body: some View {
         if entry.type == type {
             content(entry.value)
+                .onDisappear {
+                    if !navigation.path.contains(where: { e in e.id == entry.id && e.type == entry.type }) {
+                        onDismiss()
+                    }
+                }
         } else {
             EmptyView()
         }
